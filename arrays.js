eb3ebe589f8ffabc9fc4f1351f1a03a2ce6919fd
youngrob84 <youngrob84@yahoo.com>
2019-01-06 05:11:31 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,29 @@
+
+var chocolateBars=["snickers","hundred grand","kitkat","skittles" ];
+chocolateBars.push("m&m's")
+chocolateBars.unshift("twix")
+chocolateBars[1]="reese's"
+var array
+var element
+
+function addElementToBeginningOfArray (array,element){
+  var NewArray=[element, ...array]
+
+
+
+}
+
+
+function destructivelyAddElementToBeginningOfArray (array,element){
+[array].unshift(element)
+return 
+}
+
+function addElementToEndOfArray(array,element){
+  var NewArray=[...array, element]
+  return NewArray
+}
+function destuctivelyAddElementToEndOfArray(array,element){
+  array.push(element)
+return
+}
